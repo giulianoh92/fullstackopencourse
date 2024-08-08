05d0fa369924bc2c65bfa667eb250a10b946325f
giulianoh92 <giulianohillebrand@gmail.com>
2024-08-08 12:53:54 -0300

    part1 - 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,39 +6,46 @@ const Button = (props) => (
   </button>
 )
 
-const Statistics = ({ good, neutral, bad}) => (
-  <>
-    <h1>statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {good + neutral + bad}</p>
-    <p>
-      average{' '}
-      {(good + (neutral * 0) + (bad * -1)) /
-        (good + neutral + bad || 1)}
-    </p>
-    <p>
-      positive{' '}
-      {((good / (good + neutral + bad)) * 100) || 0} %
-    </p>
-  </>
-)
+const Statistics = ({ good, neutral, bad, hasFeedback }) => {
+  if (!hasFeedback) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good + (neutral * 0) + (bad * -1)) / (good + neutral + bad || 1)}</p>
+      <p>positive {((good / (good + neutral + bad)) * 100) || 0} %</p>
+    </>
+  )
+}
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [hasClicked, setHasClicked] = useState(false)
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setHasClicked(true)
   }
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setHasClicked(true)
   }
   const handleBadClick = () => {
     setBad(bad + 1)
+    setHasClicked(true)
   }
 
   return (
@@ -47,7 +54,7 @@ const App = () => {
       <Button handleClick={handleGoodClick} text="good" />
       <Button handleClick={handleNeutralClick} text="neutral" />
       <Button handleClick={handleBadClick} text="bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} hasFeedback={hasClicked} />
     </div>
   )
 }
